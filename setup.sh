118f71ba64c11109576a6517179de0df7e6eb7b0
Harry Sauers <hsauers5@gmail.com>
2019-08-03 02:01:29 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -9,4 +9,9 @@ wget https://github.com/mozilla/geckodriver/releases/download/v0.24.0/geckodrive
 sudo tar xzf geckodriver-v0.24.0-linux64.tar.gz
 sudo chmod +x geckodriver
 sudo mv geckodriver /usr/local/bin/geckodriver
+sudo fallocate -l 1.5G /swapfile
+sudo chmod 600 /swapfile
+sudo mkswap /swapfile
+sudo swapon /swapfile
+echo "/swapfile swap swap defaults 0 0" >> /etc/fstab
 python3 selenium_test.py
